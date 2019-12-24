3806f9f5d912164eaf526574705a682f4dd9e4e8
Joris Douven <joris.douven@fourtress.nl>
2019-12-24 11:06:44 +0100

    Removed publicRuntimeConfig

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,8 @@
 import { NextPage } from "next";
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
 
 const About: NextPage = () => (
   <>
     <h1>About page!</h1>
-    <p>Version: {publicRuntimeConfig.version}</p>
   </>
 );
 
